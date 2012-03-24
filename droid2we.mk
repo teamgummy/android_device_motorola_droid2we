4241d806649b3fff6e17d34a8242ce0918bc99ee
Dustin Einglett <x13thangelx@gmail.com>
2012-03-24 01:14:25 -0500

    that might cause a few problems...

--- droid2we.mk
+++ droid2we.mk
@@ -31,7 +31,11 @@ PRODUCT_COPY_FILES := $(DEVICE_PREBUILT)/etc/apns-conf.xml:system/etc/apns-conf.
 PRODUCT_PACKAGES := \
 	Camera
 
-# Audio + Bluetooth
+# Audio
+PRODUCT_COPY_FILES += \
+  device/motorola/droid2we/audio/libaudio.so:/system/lib/libaudio.so \
+  device/motorola/droid2we/audio/liba2dp.so:/system/lib/liba2dp.so
+
 PRODUCT_PACKAGES += \
 	audio.primary.omap3 \
 	libaudiohw_legacy \
