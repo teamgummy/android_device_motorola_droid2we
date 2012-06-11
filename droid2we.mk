144859e6f9f6ee1773fdc2b85c67034741919cc7
Dustin Einglett <x13thangelx@gmail.com>
2012-06-11 01:47:28 -0500

    update to current cm9

--- droid2we.mk
+++ droid2we.mk
@@ -30,6 +30,7 @@ PRODUCT_COPY_FILES += \
 	device/motorola/droid2we/audio/liba2dp.so:/system/lib/liba2dp.so
 
 PRODUCT_PACKAGES += \
+	sensors.droid2we \
 	audio.primary.omap3 \
 	libaudiohw_legacy \
 	audio.a2dp.default \
